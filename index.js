07258299020a5277c0da42ee61b1594a21efbb57
Phillip Green <phillycheez11@gmail.com>
2018-01-22 22:24:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendKitten(name){
-  kittens.unshift(name);
+  kittens.push(name);
   return kittens;
   
 }
